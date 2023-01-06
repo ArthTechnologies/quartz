210773cd415c96477b5016adcdd04ba8f16bcbe1
DiamonC <diamonc@proton.me>
2023-01-06 16:51:54 +0000

    Changed /terminal to use path and query params

--- routes/settings.js
+++ routes/settings.js
@@ -7,69 +7,66 @@ let settings = require("../stores/settings.json");
 
 let sk = require("../stores/secrets.json").stripekey;
 
-//if environment variable WEB_PORT is set, set settings.webport to that
 if (process.env.SERVERS_PER_USER) {
-  settings.serversPerUser = process.env.WEB_PORT;
+	settings.serversPerUser = process.env.WEB_PORT;
 }
 if (process.env.BROWSER_TITLE) {
-  settings.browserTitle = process.env.WEB_PORT;
+	settings.browserTitle = process.env.WEB_PORT;
 }
 
 if (process.env.WEB_NAME) {
-  settings.webName = process.env.WEB_PORT;
+	settings.webName = process.env.WEB_PORT;
 }
 if (process.env.ENABLE_PAY) {
-  settings.enablePay = process.env.WEB_PORT;
+	settings.enablePay = process.env.WEB_PORT;
 }
 if (process.env.TRUSTED_DOMAINS) {
-  settings.trustedDomains = process.env.WEB_PORT;
+	settings.trustedDomains = process.env.WEB_PORT;
 }
 
-router.get(`/`, function (req, res) {
-  if (sk.indexOf("sk") == "-1") {
-    settings.enablePay = false;
-  }
-  //add cors header
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  //return settings.json
-  res.json(settings);
+router.get(`/`, function(req, res) {
+	if (sk.indexOf("sk") == "-1") {
+		settings.enablePay = false;
+	}
+	//add cors header
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	//return settings.json
+	res.json(settings);
 });
 
-router.post(`/`, function (req, res) {
-  var keyMatch = true;
-  //add cors header
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+router.post(`/`, function(req, res) {
+	var keyMatch = true;
+	//add cors header
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 
-  settings = {
-    serversPerUser: req.body.serversPerUser,
-    browserTitle: req.body.browserTitle,
-    webLogo: req.body.webLogo,
-    enablePay: req.body.enablePay,
-    disableAuth: req.body.disableAuth,
-    trustedDomains: req.body.trustedDomains,
-  };
-  console.log(settings);
-  if (keyMatch) {
-    //write settings to settings.json
-    fs.writeFile(
-      "stores/settings.json",
-      JSON.stringify(settings),
-      function (err) {
-        if (err) throw err;
-        console.log("File is created successfully.");
-      }
-    );
-  }
-  //return settings.json
-  res.json(settings);
+	for (i in req.body) {
+		if (req.body[i] != "") {
+			let x = req.body[i];
+			settings.$i = req.body[i];
+		}
+	}
+	console.log(settings);
+	if (keyMatch) {
+		//write settings to settings.json
+		fs.writeFile(
+			"stores/settings.json",
+			JSON.stringify(settings),
+			function(err) {
+				if (err) throw err;
+				console.log("File is created successfully.");
+			}
+		);
+	}
+	//return settings.json
+	res.json(settings);
 });
 
 module.exports = router;
