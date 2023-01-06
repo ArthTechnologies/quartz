24c517e33f60b80b0792efef3f7f623c660677b5
DiamonC <diamonc@proton.me>
2023-01-06 16:47:09 +0000

    Changed /terminal to use path and query params

--- routes/terminal.js
+++ routes/terminal.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const f = require("../scripts/mc.js");
 
-router.get("/", (req, res) => {
-  res.send(f.readTerminal(req.headers.id));
+router.get("/:id", (req, res) => {
+	res.send(f.readTerminal(req.params.id));
 });
 
-router.post("/", (req, res) => {
-  f.writeTerminal(req.headers.id, req.headers.cmd);
-  res.send(req.headers.id, req.headers.cmd);
+router.post("/:id", (req, res) => {
+	f.writeTerminal(req.params.id, req.query.cmd);
+	res.send("Success");
 });
 
 module.exports = router;
