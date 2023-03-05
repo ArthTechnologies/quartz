db750f8b1bef82d03f5be7b3910ee81ce156395d
diamonc <diamonc@proton.me>
2023-03-05 11:02:35 -0600

    removed reference to v variable

--- scripts/mc.js
+++ scripts/mc.js
@@ -179,7 +179,7 @@ function run(id, software, version, addons, cmd, em, isNew, modpackURL) {
    } 
 let modpack;
 
-    exec("curl -o " + folder + "/server.jar -LO https://serverjars.com/api/fetchJar/modded/" + s + "/" + v, (error, stdout, stderr) => {
+    exec("curl -o " + folder + "/server.jar -LO https://serverjars.com/api/fetchJar/modded/" + s + "/" + version, (error, stdout, stderr) => {
       console.log("done");
     });
 console.log(modpackURL)
