f30bb34721bf7f7e337d3c5802dc2bc24c5e46a4
diamonc <diamonc@proton.me>
2023-07-15 19:07:19 -0500

    temporary debug log

--- routes/server.js
+++ routes/server.js
@@ -530,7 +530,7 @@ router.post("/:id/world", upload.single("file"), function (req, res) {
             );
             textByLine[index] = `level-seed=${req.query.seed}`;
             var newText = textByLine.join("\n");
-            console.log("seed" + textByLine[index]);
+            console.log("seed\n" + newText);
             fs.writeFile(`servers/${id}/server.properties`, newText, (err) => {
               if (err) {
                 console.log(err);
