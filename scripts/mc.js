9dbd9487707e562d84d8d83d0871e1239fafc9af
diamonc <diamonc@proton.me>
2023-07-23 23:18:36 -0400

    temporary debug log

--- scripts/mc.js
+++ scripts/mc.js
@@ -239,6 +239,7 @@ function run(id, software, version, addons, cmd, em, isNew, modpackURL) {
       "data/" + software + "-" + version + ".jar",
       folder + "/server.jar"
     );
+    console.log("test9")
   } else {
     fs.copyFileSync("data/" + software + "-0.5.1.jar", folder + "/server.jar");
     args = [
