9a96f3642b40b57cbb2810c0f51cae9505fdf108
diamonc <diamonc@proton.me>
2023-07-23 23:30:38 -0400

    debug

--- routes/server.js
+++ routes/server.js
@@ -206,12 +206,13 @@ router.post(`/new`, function (req, res) {
   email = req.headers.email;
 
   token = req.headers.token;
-
+  console.log("test9" + token === accounts[email].token &&
+  servers[req.params.id].accountId == accounts[email].accountId)
   if (
     token === accounts[email].token &&
     servers[req.params.id].accountId == accounts[email].accountId
   ) {
-    console.log("test9")
+
     let amount = f.checkServers(accounts[email].accountId).amount;
     //add cors header
     res.header("Access-Control-Allow-Origin", "*");
