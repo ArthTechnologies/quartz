3acc91a10b3f4a01316dab344ff9068e44b75209
diamonc <diamonc@proton.me>
2023-07-23 23:22:01 -0400

    debug

--- scripts/mc.js
+++ scripts/mc.js
@@ -70,6 +70,7 @@ function checkServer(id) {
 }
 
 function run(id, software, version, addons, cmd, em, isNew, modpackURL) {
+  console.log("test9")
   let out = [];
   servers = require("../servers.json");
   states[id] = "starting";
@@ -148,7 +149,7 @@ function run(id, software, version, addons, cmd, em, isNew, modpackURL) {
       c = "servers";
       break;
   }
-  console.log("test9")
+
   const settings = require("../stores/settings.json");
   let latestVersion = settings.latestVersion;
   switch (version) {
