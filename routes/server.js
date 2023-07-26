1759f9eaeb8fcf03b2b34b544871c16943812cf5
diamonc <diamonc@proton.me>
2023-07-25 20:58:12 -0400

    debug log

--- routes/server.js
+++ routes/server.js
@@ -146,7 +146,7 @@ router.get(`/:id/:modtype(plugins|mods)`, function (req, res) {
     if (modpack.files != undefined) {
       modpack.files.sort((a, b) => {
         const nameA = a.downloads[0].split("/")[4].name;
-        console.log(a);
+        console.log(a.downloads[0].split("/")[4]);
         const nameB = b.downloads[0].split("/")[4].name;
         return nameA.localeCompare(nameB);
       });
