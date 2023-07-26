e62bc43a23a7c97218e271aec6d3f88ec99c2604
diamonc <diamonc@proton.me>
2023-07-25 22:46:22 -0400

    fixed bracket never getting added

--- scripts/files.js
+++ scripts/files.js
@@ -197,7 +197,7 @@ function simplifyTerminal(terminal) {
       
 
     }
-    if (terminalLines[0] != "" && !terminal.charAt(0).includes("[")) {
+    if (terminalLines[0] != "" && !terminal.charAt(0) == '[') {
       return "["+terminalLines.join("\n[");
     }
 
