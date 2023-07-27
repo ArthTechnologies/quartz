00e3acb70d301e4dddd164bb14ba458d9814fa88
diamonc <diamonc@proton.me>
2023-07-27 13:26:54 -0500

    debug

--- scripts/files.js
+++ scripts/files.js
@@ -34,7 +34,8 @@ function readFilesRecursive(directoryPath) {
 
   files.forEach((file) => {
     const curPath = `${directoryPath}/${file}`;
-    let filePath = curPath.split("/").splice(0, 2).join("/");
+    let filePath = curPath.split("/").splice(0, 3).join("/");
+    console.log(filePath);
     if (fs.lstatSync(curPath).isDirectory()) {
       const subDir = readFilesRecursive(curPath);
       result.push([file + ":" + filePath, subDir]);
