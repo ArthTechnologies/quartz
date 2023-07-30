28185db4abb1e400322e9ff973fdd1531481347f
diamonc <diamonc@proton.me>
2023-07-30 13:02:37 -0500

    debug log

--- routes/server.js
+++ routes/server.js
@@ -1010,6 +1010,7 @@ router.post("/:id/file/:path", function (req, res) {
       fs.writeFileSync(`servers/${req.params.id}/${path}`, req.query.text);
       res.status(200).json({ msg: "Done" });
     } else {
+      console.log(fs.existsSync(`servers/${req.params.id}/${path}`));
       res.status(400).json({ msg: "Invalid request." });
     }
   } else {
