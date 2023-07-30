173c53bf6f54fc2b78011afed4dc775dec2a0ffa
diamonc <diamonc@proton.me>
2023-07-30 13:30:13 -0500

    debug log

--- routes/server.js
+++ routes/server.js
@@ -1007,7 +1007,7 @@ router.post("/:id/file/:path", function (req, res) {
       req.body !== undefined &&
       fs.existsSync(`servers/${req.params.id}/${path}`)
     ) {
-      console.log(req.body);
+      console.log(req.body.content);
       fs.writeFileSync(
         `servers/${req.params.id}/${path}`,
         JSON.stringify(req.body)
