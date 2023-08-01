882be7c60101a12bf9da3bb90ecae04abf4bc680
diamonc <diamonc@proton.me>
2023-08-01 10:44:35 -0500

    fixed numServer not working

--- routes/node.js
+++ routes/node.js
@@ -6,7 +6,7 @@ const settings = require("../stores/settings.json");
 Router.get("/", (req, res) => {
   res.status(200).json({
     maxServers: settings.maxServers,
-    numServer: Object.keys(settings).length - 1,
+    numServers: Object.keys(servers).length - 1,
   });
 });
 
