ee6e0f04f17dc8627420808977f19f6eafa513a5
diamonc <diamonc@proton.me>
2023-08-01 10:45:02 -0500

    fixed servers not being defined

--- routes/node.js
+++ routes/node.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Router = express.Router();
 const fs = require("fs");
 const settings = require("../stores/settings.json");
+const servers = require("../servers.json");
 
 Router.get("/", (req, res) => {
   res.status(200).json({
