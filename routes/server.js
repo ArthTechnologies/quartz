ccb69acf384d4a68c3e6728ca5edd16368169ed9
diamonc <diamonc@proton.me>
2023-08-04 09:25:50 -0500

    removed debug log

--- routes/server.js
+++ routes/server.js
@@ -230,9 +230,7 @@ router.post(`/new`, function (req, res) {
     };
 
     let cid = "";
-    console.log(
-      (stripekey.indexOf("sk") == -1) | (account.bypassStripe == true) && true
-    );
+
     if (
       (stripekey.indexOf("sk") == -1 || account.bypassStripe == true) &&
       (settings.maxServers > data.numServers ||
