f6d20433fd202c36671d42f1ebd82dc1c654f075
diamonc <diamonc@proton.me>
2023-08-04 13:44:14 -0500

    fixed id being incorrect

--- routes/server.js
+++ routes/server.js
@@ -215,7 +215,10 @@ router.post(`/new`, function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     const settings = require("../stores/settings.json");
     //1 is subtracted because of the "template" subdirectory
-    var id = fs.readdirSync("servers").length;
+    var id =
+      parseInt(
+        fs.readdirSync("servers")[fs.readdirSync("servers").length - 2]
+      ) + 1;
     const datajson = require("../stores/data.json");
     datajson.numServers = id;
     fs.writeFileSync("data.json", JSON.stringify(datajson, null, 2));
