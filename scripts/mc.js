7d38c7f659cfdb7049bba6b481f77f6112562339
diamonc <diamonc@proton.me>
2023-08-04 09:29:03 -0500

    debug log

--- scripts/mc.js
+++ scripts/mc.js
@@ -90,7 +90,7 @@ function checkServer(id) {
 }
 
 function run(id, software, version, addons, cmd, em, isNew, modpackURL) {
-  console.log(id);
+  console.log(fs.readFileSync("servers/" + id + "/server.json"));
   let server = require("../servers/" + id + "/server.json");
   let out = [];
   states[id] = "starting";
