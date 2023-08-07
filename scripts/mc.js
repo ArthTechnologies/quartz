2e75ad745a0fb15e2fcfc5ddac4cd5980246d20e
diamonc <diamonc@proton.me>
2023-08-07 12:03:30 -0500

    fixed syntax error

--- scripts/mc.js
+++ scripts/mc.js
@@ -458,7 +458,7 @@ function run(id, software, version, addons, cmd, em, isNew, modpackURL) {
     ls.on("exit", () => {
       states[id] = "false";
       terminalOutput[id] = out.join("\n");
-      console.log("DEBUGDEBUGDEBUG" + terminalOutput[id];
+      console.log("DEBUGDEBUGDEBUG" + terminalOutput[id]);
     });
   }
 
