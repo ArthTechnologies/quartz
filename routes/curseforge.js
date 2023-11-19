86d17b07ae4fb6aec62e778a3d0a5f2446ff7ab3
DiamonC <diamonc@proton.me>
2023-11-19 13:11:34 -0600

    added versions route

--- routes/curseforge.js
+++ routes/curseforge.js
@@ -94,4 +94,30 @@ Router.get("/:id/description", (req, res) => {
     );
   }
 });
+
+Router.get("/:id/versions", (req, res) => {
+  if (apiKey != undefined) {
+    let id = req.params.id;
+    const exec = require("child_process").exec;
+    exec(
+      `curl -X GET "https://api.curseforge.com/v1/mods/${id}/files"` +
+        ` -H 'x-api-key: ${apiKey}'`,
+      (error, stdout, stderr) => {
+        if (
+          !error &&
+          stdout != undefined &&
+          JSON.parse(stdout).data != undefined
+        ) {
+          try {
+            res.status(200).json(JSON.parse(stdout).data);
+            } catch {
+              res.status(400).json({msg:"Error parsing JSON."})
+            }
+        } else {
+          res.status(500).json({ msg: "Internal server error." });
+        }
+      }
+    );
+  }
+});
 module.exports = Router;
