c974ece65d48636afc6610a4a75872fa618f245c
diamonc <diamonc@proton.me>
2023-12-01 10:43:16 -0600

    added route to get a changelog

--- routes/curseforge.js
+++ routes/curseforge.js
@@ -120,4 +120,31 @@ Router.get("/:id/versions", (req, res) => {
     );
   }
 });
+
+Router.get("/:id/version/:versionId/changelog", (req, res) => {
+  if (apiKey != undefined) {
+    let id = req.params.id;
+    let versionId = req.params.versionId;
+    const exec = require("child_process").exec;
+    exec(
+      `curl -X GET "https://api.curseforge.com/v1/projects/${id}/files/${versionId}/changelog"` +
+        ` -H 'x-api-key: ${apiKey}'`,
+      (error, stdout, stderr) => {
+        if (
+          !error &&
+          stdout != undefined &&
+          JSON.parse(stdout).data != undefined
+        ) {
+          try {
+            res.status(200).json(JSON.parse(stdout).data);
+            } catch {
+              res.status(400).json({msg:"Error parsing JSON."})
+            }
+        } else {
+          res.status(500).json({ msg: "Internal server error." });
+        }
+      }
+    );
+  }
+});
 module.exports = Router;
