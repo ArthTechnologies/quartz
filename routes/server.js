55750bcb4913e34ee3fa3d8e03dc239bd5a45ad9
diamonc <diamonc@proton.me>
2023-12-06 12:35:32 -0600

    debug log

--- routes/server.js
+++ routes/server.js
@@ -867,6 +867,7 @@ router.post("/:id/world", upload.single("file"), function (req, res) {
             var newText = textByLine.join("\n");
             fs.writeFileSync(`servers/${id}/server.properties`, newText);
             files.removeDirectoryRecursiveAsync(`servers/${id}/world`, () => {
+              console.log("debug log")
               fs.mkdirSync(`servers/${id}/world`);
               fs.mkdirSync(`servers/${id}/world/datapacks`);
               files.removeDirectoryRecursive(`servers/${id}/world_nether`);
@@ -874,7 +875,7 @@ router.post("/:id/world", upload.single("file"), function (req, res) {
               //unzip the file and put it in /servers/id/world
 
               const exec = require("child_process").exec;
-              console.log("debug log")
+
               if (enableVirusScan) {
                 console.log(req.file.path)
                 exec(
