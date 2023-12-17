3ce5b3ccd796b0c08219fd7939d014393606acd7
DiamonC <diamonc@proton.me>
2023-12-16 23:17:56 -0600

    changed post route to use paymentIntents

--- routes/checkout.js
+++ routes/checkout.js
@@ -16,29 +16,20 @@ Router.post("/:plan", async (req, res) => {
     priceId = "price_1OLaahJYPXquzaSzTKOn08Rn";
   }
 
-  const session = await stripe.checkout.sessions.create({
-    ui_mode: "embedded",
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: priceId,
-        quantity: quantity,
-      },
-    ],
-    mode: "subscription",
-    return_url: `https://servers.arthmc.xyz/subscription-success`,
-    automatic_tax: { enabled: true },
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
   });
 
-  res.send({ clientSecret: session.client_secret });
-});
-
-Router.get("/status", async (req, res) => {
-  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
-
   res.send({
-    status: session.status,
-    customer_email: session.customer_details.email,
+    clientSecret: paymentIntent.client_secret,
   });
 });
 
