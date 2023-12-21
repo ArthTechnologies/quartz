96af188afcac5cd451dd6fde4c0820f320451762
DiamonC <diamonc@proton.me>
2023-12-21 13:16:27 -0600

    fixed args messing up forge

--- scripts/mc.js
+++ scripts/mc.js
@@ -367,6 +367,9 @@ function run(
       if (doneInstalling & timeToLoad) {
         timeToLoad = false;
         states[id] = "starting";
+
+        let args =
+          "-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs";
         //-Dlog4j.configurationFile=consoleconfig.xml
         //get the forge version from the name of the folder inside /libraries/net/minecraftforge/forge/
 
