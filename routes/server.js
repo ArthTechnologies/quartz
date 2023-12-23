50e278944f37b233b8d77f73c11c13388c29a337
DiamonC <diamonc@proton.me>
2023-12-23 17:20:23 -0600

    debug log

--- routes/server.js
+++ routes/server.js
@@ -121,13 +121,14 @@ router.get(`/claimId`, function (req, res) {
       }
       if (id != -1 && id < config.maxServers) {
         if (account.servers == undefined) account.servers = [];
-        console.log("debug " + account.servers);
+
         account.servers.push(id);
-        console.log("debug " + account.servers);
+        console.log(account);
         fs.writeFileSync(
           "accounts/" + email + ".json",
           JSON.stringify(account, null, 4)
         );
+        console.log(fs.readFileSync("accounts/" + email + ".json"));
         res.status(200).json({ id: id });
       } else {
         res.status(400).json({
