cbd666bc8b1986bd07d23381c57bcfd7c98b5c8e
DiamonC <diamonc@proton.me>
2024-03-21 20:28:33 -0500

    removed debug log fixed it actually this time

--- routes/curseforge.js
+++ routes/curseforge.js
@@ -15,21 +15,7 @@ Router.get("/search", (req, res) => {
     let categories = req.query.categories || "";
     console.log(index);
     const exec = require("child_process").exec;
-    console.log(
-      "request url = " +
-        `curl -X GET "https://api.curseforge.com/v1/mods/search` +
-        `?gameId=432` +
-        `&gameVersion=${gameVersion}` +
-        `&modLoaderType=${modLoaderType}` +
-        `&searchFilter=${searchFilter}` +
-        `&classId=${classId}` +
-        `&index=${index}` +
-        `&pageSize=15` +
-        `&sortField=${sortField}` +
-        `&sortOrder=desc` +
-        `&categoryIds=[${categories}]"` +
-        ` -H 'x-api-key: ${apiKey}'`
-    );
+
     exec(
       `curl -X GET "https://api.curseforge.com/v1/mods/search` +
         `?gameId=432` +
@@ -41,7 +27,7 @@ Router.get("/search", (req, res) => {
         `&pageSize=15` +
         `&sortField=${sortField}` +
         `&sortOrder=desc` +
-        `&categoryIds=[${categories}]"` +
+        `&categoryIds=${categories}"` +
         ` -H 'x-api-key: ${apiKey}'`,
       (error, stdout, stderr) => {
         if (!error && stdout != undefined) {
