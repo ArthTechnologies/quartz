d83412e789cec1ccb536f8214e6d0906303cba53
diamonc <diamonc@proton.me>
2024-03-27 12:05:14 -0500

    added promo codes, langs, custom currencies to checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -6,7 +6,10 @@ const config = require("../scripts/utils.js").getConfig();
 
 Router.post("/:plan", async (req, res) => {
   let plan = req.params.plan;
-  let quantity = req.query.quantity;
+  let quantity = req.query.quantity || 1;
+  let customer_email = req.query.customer_email || null;
+  let currency = req.query.currency || "usd";
+  let locale = req.query.locale || "en";
   if (quantity == undefined) {
     quantity = 1;
   }
@@ -19,6 +22,10 @@ Router.post("/:plan", async (req, res) => {
 
   const session = await stripe.checkout.sessions.create({
     ui_mode: "embedded",
+    customer_email: customer_email,
+    currency: currency,
+    locale: locale,
+    allow_promo_codes: true,
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
