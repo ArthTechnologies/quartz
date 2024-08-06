fe508e0ce5cd87049927ad23e6734e3a266db910
DiamonC <diamonc@proton.me>
2024-08-05 19:48:35 -0500

    debug

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -145,12 +145,9 @@ Router.get("/servers", async (req, res) => {
           let pid = execSync(
             "lsof -i :" + (10000 + parseInt(serverId)) + " -t"
           );
-          console.log(
-            `cat /proc/${pid}/status | grep -i vmrss | awk '{print $2}'`
-          );
-          memory = execSync(
-            `cat /proc/${pid}/status | grep -i vmrss | awk '{print $2}'`
-          );
+          let string = `cat /proc/${pid}/status | grep -i vmrss | awk '{print $2}'`;
+          console.log(string);
+          memory = execSync(string);
         } catch (e) {
           console.log(e);
         }
