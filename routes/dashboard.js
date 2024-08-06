cf86f314767abbd7d7036a2495baee6d9c010bbb
DiamonC <diamonc@proton.me>
2024-08-05 19:56:50 -0500

    test

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -145,7 +145,7 @@ Router.get("/servers", async (req, res) => {
           let pid = execSync(
             "lsof -i :" + (10000 + parseInt(serverId)) + " -t"
           );
-          let string = `ps -p ${pid} -o rss=`;
+          let string = "ps -p "+pid+" -o rss=`;
           console.log(string);
           memory = execSync(string);
         } catch (e) {
