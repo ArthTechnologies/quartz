09510d5732362487b4a0355287bec71ba011438a
DiamonC <diamonc@proton.me>
2024-10-12 16:23:30 -0500

    updated sh

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Check if the script is run as root
-if [[ "$EUID" -ne 0 ]]; then
-  echo "This script must be run as root. Please use sudo."
-  exit 1
-fi
-
-# Start the screen session
 screen -dmS qua sh scripts/autorestart.sh
 
 echo "Quartz has started at port 4000."
