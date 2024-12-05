ac3ab719ed7d91b991e6b614bb6be8974c2479a0
DiamonC <diamonc@proton.me>
2024-12-05 17:54:42 -0600

    added docker sql started to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker run -d --name db -p 8080:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=drawsql -v db_data:/var/lib/mysql --restart always mysql:5.7
+
 screen -dmS qua sh scripts/autorestart.sh
 
 echo "Quartz has started at port 4000."
