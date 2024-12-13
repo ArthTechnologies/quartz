b406c068e5f27d894d4f3c6d2531a02c7021bc88
DiamonC <diamonc@proton.me>
2024-12-12 21:07:30 -0600

    improved accounts tsv migration function

--- scripts/migrations.js
+++ scripts/migrations.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const { readJSON } = require("./utils.js");
 function accountsToTSV() {
     let accounts = fs.readdirSync("accounts");
-    let columns = ["id","username","billingEmail","servers","stripeServers","freeServers","password","salt","token","resetAttempts"];
+    let columns = ["id","username","billingEmail","servers","stripeServers","freeServers","lastSignin","token","salt","password","resetAttempts"];
     let tsv = columns.join("\t") + "\n";
     for (let i in accounts) {
         if (accounts[i].includes(".json")) {
         let account = readJSON(`accounts/${accounts[i]}`);
         try {
-        let row = [account.accountId, accounts[i].split(".json")[0], account.email, account.servers, 0, account.freeServers, account.password, account.salt, account.token, account.resetAttempts];
+        let row = [account.accountId, accounts[i].split(".json")[0], account.email, account.servers, 0, account.freeServers, account.lastSignin, account.token, account.salt, account.password, account.resetAttempts];
         tsv += row.join("\t") + "\n";
         } catch (e) {
         console.log("error", e);
