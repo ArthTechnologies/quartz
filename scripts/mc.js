949914f68c76e74e544c005ddb10eb5fb7880d72
DiamonC <diamonc@proton.me>
2025-01-01 19:31:33 -0600

    d

--- scripts/mc.js
+++ scripts/mc.js
@@ -243,7 +243,7 @@ function run(
     console.log("absolutePath: " + absolutePath);
 
     let port = portOffset + parseInt(id);
-    let prefix = `docker run -m ${allocatedRAM}g -i -v ${absolutePath}/servers/${id}:/server -w /server -p ${port}:${port}/tcp -p ${port}:${port}/udp -p ${port + 200}:${port + 200}/tcp -p ${port + 300}:${port + 300}/udp --user 1000:1000 openjdk:${javaVer} java`;
+    let prefix = `docker run -m ${allocatedRAM}g -i -v ${absolutePath}/servers/${id}:/server -w /server -p ${port}:${port}/tcp -p ${port}:${port}/udp -p ${port + 200}:${port + 200}/tcp -p ${port + 325}:${port + 325}/udp --user 1000:1000 openjdk:${javaVer} java`;
     console.log("prefix: " + prefix);
 
     let doneInstallingServer = false;
