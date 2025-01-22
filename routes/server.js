1b7df0bb8a9ea23c17017987eda8103c58664b28
DiamonC <diamonc@proton.me>
2025-01-21 21:35:41 -0600

    imported path

--- routes/server.js
+++ routes/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const files = require("../scripts/files.js");
 const f = require("../scripts/mc.js");
