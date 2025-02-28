77e634b857079ccec153b2f159e555c82db85cad
DiamonC <diamonc@proton.me>
2025-02-27 19:18:55 -0600

    debug log

--- scripts/ftp.js
+++ scripts/ftp.js
@@ -31,14 +31,14 @@ let port = 10000 + parseInt(config.idOffset) + 99;
 
 let mountArray = [];
 let usersArray = [];
-for (let i = 0; i < users.length; i++) {
+for (let i = 0; i < 3; i++) {
     mountArray.push(`-v "${users[i].split(":")[2]}:/home/username/server-${users[i].split(":")[3]}" `);
     usersArray.push(`"${users[i].split(":")[0]}:${users[i].split(":")[1]}:::server-${users[i].split(":")[3]}" `);
 }
 console.log(mountArray);
 function startFtpServer() {
     const { exec} = require('child_process');
-    //console.log(`docker run -d --name sftp_server -p ${port}:22 ${mountArray.join(" ")}atmoz/sftp ${usersArray.join(" ")}`);
+    console.log(`docker run -d --name sftp_server -p ${port}:22 ${mountArray.join(" ")}atmoz/sftp ${usersArray.join(" ")}`);
     exec(`docker run -d --name sftp_server -p ${port}:22 ${mountArray.join(" ")}atmoz/sftp ${usersArray.join(" ")}`, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error starting FTP server: ${error}`);
