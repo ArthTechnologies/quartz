1066d3022964ae5cb850ec21c449cb3c22b2f4b8
DiamonC <diamonc@proton.me>
2025-03-05 12:50:59 -0600

    limited cpu usage to 1 core/thread

--- scripts/mc.js
+++ scripts/mc.js
@@ -255,7 +255,7 @@ function run(
     }
 
     let port = portOffset + parseInt(id);
-    let prefix = `docker run -m ${allocatedRAM}g -i -v ${absolutePath}/servers/${id}:/server -w /server -p ${port}:${port}/tcp -p ${port}:${port}/udp -p ${port + 66}:${port + 66}/tcp -p ${port + 33}:${port + 33}/udp --user 1000:1000 openjdk:${javaVer} java`;
+    let prefix = `docker run -m ${allocatedRAM}g -i -v ${absolutePath}/servers/${id}:/server -w /server -p ${port}:${port}/tcp -p ${port}:${port}/udp -p ${port + 66}:${port + 66}/tcp -p ${port + 33}:${port + 33}/udp --user 1000:1000 --cpus="1" openjdk:${javaVer} java`;
     console.log("prefix: " + prefix);
 
     let doneInstallingServer = false;
