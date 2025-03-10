d7e611a28f9753e1b2ab79ada763c956da9fff55
DiamonC <diamonc@proton.me>
2025-03-09 20:42:54 -0500

    debug log

--- scripts/ftp.js
+++ scripts/ftp.js
@@ -35,7 +35,7 @@ let mountArray = [];
 let usersArray = [];
 try {
     for (let i = 0; i < users.length; i++) {
-        mountArray.push(`-v "${users[i].split(":")[2]}:/home/${users[i].split(":")[1]}/server `);
+        mountArray.push(`-v "${users[i].split(":")[2]}:/home/${users[i].split(":")[0]}/server `);
         usersArray.push(`"${users[i].split(":")[0]}:${users[i].split(":")[1]}:::server `);
     }
 } catch (e) {
