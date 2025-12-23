d64ca7c3ea7e181f5a0693620afc040fa57a410d
diamonc <diamonc@proton.me>
2025-12-23 03:55:57 +0000

    Revert "fixed ftp password undefined"
    This reverts commit b7c8af5afe690a427c6ff8fb4aaed9010f2698ea.

--- scripts/ftp.js
+++ scripts/ftp.js
@@ -106,15 +106,7 @@ function startFtpServer() {
 }
 
 function getTempToken(username) {
-  // username format: "ACCID.SERVERID"
-  const serverId = username.split('.')[1];
-  if (!serverId) return null;
-  try {
-    return security.getFileAccessKey(serverId);
-  } catch (e) {
-    console.error(`Error getting FTP token for ${username}:`, e);
-    return null;
-  }
+  return users.find(user => user.startsWith(username)).split(':')[1];
 }
 
 module.exports = {
